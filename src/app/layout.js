26f1dc889faa48c24373162b56dffe9235c93f0a
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-08-12 05:16:22 +0100

    extention added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer';
+import Header from '../components/Header/Header.jsx';
+import Footer from '../components/Footer.jsx';
 
 
 
