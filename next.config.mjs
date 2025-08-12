bda6b0f2d7e8761ccbe5a21300f1903c0be07c2a
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-08-12 07:40:17 +0100

    oluwamomi

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-const path = require('path');
+import path from 'path';
 
-module.exports = {
+const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     return config;
   }
 };
+
+export default nextConfig;
